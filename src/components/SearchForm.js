46eb16168ffecdd808c0a86c63b088c307cfd03d
KowshickChowdhury <kowshick1170@gmail.com>
2024-05-11 01:42:51 +0600

    update search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,23 +5,40 @@ function SearchForm() {
   const [destinationAirport, setDestinationAirport] = useState("");
   const [departureDate, setDepartureDate] = useState("");
   const [returnDate, setReturnDate] = useState("");
-  const [passengerCount, setPassengerCount] = useState(1);
+  const [passengerTypes, setPassengerTypes] = useState([
+    { type: "ADT", count: 1 },
+  ]);
+  const passengerOptions = ["ADT", "CHILD", "KIDS"];
 
   const handleSubmit = () => {
 
-  }
+  };
+
+  const handlePassengerTypeChange = (index, value) => {
+    const updatePassengerTypes = [...passengerTypes];
+    updatePassengerTypes[index].type = value;
+    setPassengerTypes(updatePassengerTypes);
+  };
+
+  const handlePassengerCountChange = (index, value) => {
+    const updatePassengerCounts = [...passengerTypes];
+    updatePassengerCounts[index].count = value;
+    setPassengerTypes(updatePassengerCounts);
+  };
+
+  console.log("first", passengerTypes);
 
   return (
     <>
-      <form className="max-w-lg mx-auto mt-8" onSubmit={handleSubmit}>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+      <form className="max-w-5xl mx-auto mt-8" onSubmit={handleSubmit}>
+        <div className="flex gap-4 justify-between items-center">
           <div>
             <input
               type="text"
               id="departureAirport"
               value={departureAirport}
               onChange={(e) => setDepartureAirport(e.target.value)}
-              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded border p-2"
               placeholder="Departure Airport..."
             />
           </div>
@@ -31,7 +48,7 @@ function SearchForm() {
               id="destinationAirport"
               value={destinationAirport}
               onChange={(e) => setDestinationAirport(e.target.value)}
-              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded p-2 border"
               placeholder="Destination Airport"
             />
           </div>
@@ -47,7 +64,7 @@ function SearchForm() {
               id="departureDate"
               value={departureDate}
               onChange={(e) => setDepartureDate(e.target.value)}
-              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded p-2 border"
             />
           </div>
           <div>
@@ -62,24 +79,35 @@ function SearchForm() {
               id="returnDate"
               value={returnDate}
               onChange={(e) => setReturnDate(e.target.value)}
-              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded p-2 border"
             />
           </div>
           <div>
-            <label
-              htmlFor="passengerCount"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Passengers
-            </label>
-            <input
-              type="number"
-              id="passengerCount"
-              value={passengerCount}
-              onChange={(e) => setPassengerCount(parseInt(e.target.value))}
-              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-              min="1"
-            />
+            {passengerTypes.map((passengerType, index) => (
+              <div key={index} className="flex gap-2 items-center">
+                <select
+                className="border rounded p-2"
+                  value={passengerType.type}
+                  onChange={(e) =>
+                    handlePassengerTypeChange(index, e.target.value)
+                  }
+                >
+                  {passengerOptions.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+                <input
+                className="border rounded p-2 w-20"
+                  type="number"
+                  value={passengerType.count}
+                  onChange={(e) =>
+                    handlePassengerCountChange(index, e.target.value)
+                  }
+                />
+              </div>
+            ))}
           </div>
         </div>
         <button
@@ -91,6 +119,6 @@ function SearchForm() {
       </form>
     </>
   );
-};
+}
 
 export default SearchForm;
